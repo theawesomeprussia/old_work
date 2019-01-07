7b3683041d7ef46d807bc1dad1f4c1a1e80b9c40
theawesomeprussia <46429505+theawesomeprussia@users.noreply.github.com>
2019-01-06 19:10:53 -0500

    Create if_statements.py

---
+++ if_statements.py
@@ -0,0 +1,4 @@
+user_press_u= input('aloha brohas if you desire to know about your future... press u.') 
+
+if user_press_u == 'u':
+  print('ihave a  very specific set of skills, skills i have aquired over a long periog of time,in real estate, i will look for you iwill find you and i will build a house around you... with no walls.')
