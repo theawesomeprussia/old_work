c54c5e1a22f5b5d3a6108ebdc95f41dd2c681862
Shafay <46429505+theawesomeprussia@users.noreply.github.com>
2019-01-31 14:12:03 -0500

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,16 @@
+# in process
+
+print("boi")
+
+name = input("What's your name ")
+
+if name == "shafay":
+  print("boi thats not your name thats my name")
+
+for count in range (10,0,-1):
+  print(count)
+
+print("hang man game")
+
+attempts=20
+secretword="gilbird"
